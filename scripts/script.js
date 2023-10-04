7a926ed58639d7bb9a0d57e403cd61a89cc30f89
sloos318 <sybren.loos@gmail.com>
2023-10-04 17:19:06 +0200

    alt text en comments

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,17 @@ console.log(openButtonMenu);
 var closeButtonMenu = document.querySelector("nav button:first-of-type");
 
 var hamburgerMenu = document.querySelector("header nav:first-of-type");
+// hierboven maken we een aantal variabelen aan, een is voor de knop die de actie moet laten beginnen, een is voor de knop in de nav die 
+// het hamburgermenu af gaat sluiten en de laatste roept het hamburger menu zelf aan.
 
 openButtonMenu.onclick = openMenu;
-
+// hiermee geven we aan dat met een click een functie moet beginnen
 
 function openMenu () {
     
     hamburgerMenu.classList.add("open");
 }
+// dit is de functie die de hamburger open maakt. Dit doet die door de geclasseerde nav.open aan te zetten.
 
 
 closeButtonMenu.onclick = closeMenu;
@@ -20,6 +23,8 @@ closeButtonMenu.onclick = closeMenu;
 function closeMenu() {
     hamburgerMenu.classList.remove("open");
 }
+
+// hier haal je het hamburgermenu weer weg.
 
 var openButtonMenu2 = document.querySelector("header button:nth-of-type(2)");
 
@@ -42,9 +47,13 @@ function closeMenu2() {
     hamburgerMenu2.classList.remove("opennummertwee");
 }
 
+// hier maken we een aantal constanten aan van de classes die we hebben gemaakt in de index.
 const videoPlayer = document.querySelector(".video-player")
+// de eerste gaat om de hele video en knop heen
 const video = videoPlayer.querySelector(".video")
+// dit is de video zelf
 const playButton = videoPlayer.querySelector(".play-button")
+// dit is de knop waarmee we het af kunnen laten spelen en stop kunnen zetten
 
 playButton.addEventListener("click", (e) => {
     if(video.paused){
@@ -55,3 +64,5 @@ playButton.addEventListener("click", (e) => {
         e.target.textContent = "▷"
     }
 })
+// hier zetten we een addeventlistener op de knop. Die blijft altijd luisteren of er gedrukt is. We geven dan aan met (e) dat er een event gebeurt op de click
+// daarna geven we met een if, else statement aan dat de video op de click aan of uit gezet moet worden en dat de text van de knop moet veranderen 
